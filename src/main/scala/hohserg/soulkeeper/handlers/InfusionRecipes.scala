426878324d7abb43aa882f61e10904955f3a70eb
hohserg <hohserg1@yandex.ru>
2022-01-10 06:56:16 +0300

    Update InfusionRecipes.scala

--- src/main/scala/hohserg/soulkeeper/handlers/InfusionRecipes.scala
+++ src/main/scala/hohserg/soulkeeper/handlers/InfusionRecipes.scala
@@ -1,17 +1,20 @@
 package hohserg.soulkeeper.handlers
 
 import hohserg.soulkeeper.api.crafting.{DummyInfuserRecipe, InfuserRecipe, StepInfuserRecipe}
-import hohserg.soulkeeper.blocks.BlockDarkRhinestonePowder
+import hohserg.soulkeeper.blocks.{BlockDarkRhinestonePowder, BlockRhOrb, BlockRhPowderOrb}
 import hohserg.soulkeeper.items.ItemRhShield
 import hohserg.soulkeeper.items.bottle.{ItemDustBottle, ItemEmptyBottle, ItemFilledBottle}
 import hohserg.soulkeeper.items.tools._
 import hohserg.soulkeeper.{Configuration, Main}
+import net.minecraft.block.Block
 import net.minecraft.item.{Item, ItemStack}
 import net.minecraft.util.ResourceLocation
 import net.minecraftforge.event.RegistryEvent
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import net.minecraftforge.registries.RegistryBuilder
+
+import scala.language.implicitConversions
 
 @EventBusSubscriber(modid = Main.modid)
 object InfusionRecipes {
@@ -34,9 +37,12 @@ object InfusionRecipes {
     event.getRegistry.register(toolInfusion(ItemRhShovel))
     event.getRegistry.register(toolInfusion(ItemRhSword))
     event.getRegistry.register(toolInfusion(ItemRhShield))
-    event.getRegistry.register(StepInfuserRecipe(Item.getItemFromBlock(BlockDarkRhinestonePowder), 10, 15).setRegistryName("powder_infusion"))
+    event.getRegistry.register(StepInfuserRecipe(BlockDarkRhinestonePowder, 10, 15).setRegistryName("powder_infusion"))
     event.getRegistry.register(DummyInfuserRecipe(ItemDustBottle, new ItemStack(ItemEmptyBottle), 1).setRegistryName("bottle_infusion"))
+    event.getRegistry.register(DummyInfuserRecipe(BlockRhPowderOrb, new ItemStack(BlockRhOrb), 50).setRegistryName("bottle_infusion"))
     event.getRegistry.register(DummyInfuserRecipe(ItemEmptyBottle, new ItemStack(ItemFilledBottle), Configuration.rhinestoneBottleCapacity).setRegistryName("bottle_filling"))
   }
+
+  private implicit def blockToItem(block:Block):Item = Item.getItemFromBlock(block)
 
 }
