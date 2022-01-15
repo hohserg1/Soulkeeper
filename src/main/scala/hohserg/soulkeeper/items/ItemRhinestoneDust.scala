c4fbde16b65d191f310e05a0f61a27a71f23dff5
hohserg <hohserg1@yandex.ru>
2022-01-16 01:49:02 +0300

    Added enchanted book to creative tab

--- src/main/scala/hohserg/soulkeeper/items/ItemRhinestoneDust.scala
+++ src/main/scala/hohserg/soulkeeper/items/ItemRhinestoneDust.scala
@@ -1,5 +1,19 @@
 package hohserg.soulkeeper.items
 
-import net.minecraft.item.Item
+import hohserg.soulkeeper.enchantments.{EnchantInspire, EnchantShiny, EnchantXPLeak}
+import hohserg.soulkeeper.handlers.Registration
+import net.minecraft.creativetab.CreativeTabs
+import net.minecraft.enchantment.{EnchantmentData, EnchantmentHelper}
+import net.minecraft.item.{Item, ItemEnchantedBook, ItemStack}
+import net.minecraft.util.NonNullList
 
-object ItemRhinestoneDust extends Item
+object ItemRhinestoneDust extends Item{
+  override def getSubItems(tab: CreativeTabs, items: NonNullList[ItemStack]): Unit = {
+    super.getSubItems(tab, items)
+    if(tab==Registration.tab){
+      items.add(ItemEnchantedBook.getEnchantedItemStack(new EnchantmentData(EnchantShiny,1)))
+      items.add(ItemEnchantedBook.getEnchantedItemStack(new EnchantmentData(EnchantInspire,1)))
+      items.add(ItemEnchantedBook.getEnchantedItemStack(new EnchantmentData(EnchantXPLeak,3)))
+    }
+  }
+}
